5edbb3728dd2a6186448a22763b178e10cf7d94e
RAM007 <151729806+ram0069@users.noreply.github.com>
2024-08-26 18:43:18 +0530

    zzz

--- shivu/config.py
+++ shivu/config.py
@@ -5,8 +5,8 @@ class Config(object):
     OWNER_ID = "7011990425"
     sudo_users = ["6890857225", "7011990425", "6264760837", "5527022252", "6384484443", "1850686769"]
     GROUP_ID = "-1002041586214"
-    TOKEN = "6501935889:AAFJLFumJK7hiOa04qqx8TVb_gLxI6oT-CM"
-    mongo_url = "mongodb+srv://vinamra523:duXNC3pKUddFNb1p@newbhainew.3ioyzuw.mongodb.net/?retryWrites=true&w=majority"
+    TOKEN = "your bot token"
+    mongo_url = "your db"
     PHOTO_URL = ["https://telegra.ph/file/7e5398823512d307128a3.jpg", "https://telegra.ph/file/c45dcb207d81e97cb4f6a.jpg", "https://telegra.ph/file/0bc6d65878e8300fbf0f8.jpg", "https://telegra.ph/file/0afb45203ff162ee7227b.jpg"]
     SUPPORT_CHAT = "lustsupport"
     UPDATE_CHAT = "lustxUpdate"
